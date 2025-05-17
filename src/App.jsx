96541c3169769e6ce0559268f1afec2cdcf1ef5c
shreekeshmurkar <shreekeshmurkar@gmail.com>
2025-05-17 15:56:18 +0530

    Added temp structure for game board

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,22 @@
 function App() {
   
   return (
-    <p>Coming soon...</p>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        GAME BOARD
+      </div>
+      LOGS
+    </main>
   )
 }
 
