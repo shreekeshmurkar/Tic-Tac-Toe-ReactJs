fd5dbd9714dab2fee17c7f2bd9b3dfea27bd6352
shreekeshmurkar <shreekeshmurkar@gmail.com>
2025-05-17 19:07:21 +0530

    added state in Player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,29 @@
-function Player ({name, symbol}) {
-    return (
-      <li>
-        <span>
-          <span className="player-name">{name}</span>
-          <span className="player-symbol">{symbol}</span>
-        </span>
-        <button>Edit</button>
-      </li>
-    );
+import { useState } from "react";
+
+function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+  function handleEditClick() {
+    setIsEditing(editing => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+
+  }
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  if (isEditing) {
+    editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
+  }
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
 }
 
 export default Player;
